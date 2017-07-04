46e990a8708db43835df52d5769188f3bc03d725
Sumit Ridhal <sumitridhal@gmail.com>
2017-07-05 00:16:11 +0530

    Delete audio_metadata.js

--- audio_metadata.js
+++
@@ -1,7 +0,0 @@
-var audioMetaData = require('audio-metadata'),
-    fs = require('fs')
-
-var file = fs.readFileSync('song.mp3');
-var metadata = audioMetaData.id3v2(file);
-
-console.log(metadata);
